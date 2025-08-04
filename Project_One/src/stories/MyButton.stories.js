f3237d6655a6c5159fca1f20d6e460be0bdae07e
Johnson Bhaskaran <johnsonb@live.in>
2025-08-04 19:27:10 +0530

    Created first component My Button

---
+++ Project_One/src/stories/MyButton.stories.js
@@ -0,0 +1,16 @@
+import MyButton from "../components/Button";
+
+export default {
+  title: "My/Button",
+  component: MyButton,
+  parameters: {
+    layout: "centered",
+  },
+};
+
+export const Green = {
+  args: {
+    label: "Press me",
+    backgroundColor: "seagreen",
+  },
+};
